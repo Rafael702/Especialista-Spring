6fa18fdfb8fc24b0063953c9de30ae4c26ac66c8
Rafael <rafaeldealmeida.192001@gmail.com>
2023-02-22 10:19:07 -0300

    Implementacao do padrao repository

--- Introducao a JPA e Hibernate/Arquivo Manipulavel/algafood-api/src/main/java/com/algaworks/algafood/jpa/RemoveCozinhaMain.java
+++ Introducao a JPA e Hibernate/Arquivo Manipulavel/algafood-api/src/main/java/com/algaworks/algafood/jpa/RemoveCozinhaMain.java
@@ -2,6 +2,7 @@ package com.algaworks.algafood.jpa;
 
 import com.algaworks.algafood.AlgafoodApiApplication;
 import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -13,11 +14,11 @@ public class RemoveCozinhaMain {
                 .web(WebApplicationType.NONE)
                 .run(args);
 
-        CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+        CozinhaRepository cozinhaRepository = applicationContext.getBean(CozinhaRepository.class);
 
         Cozinha cozinha = new Cozinha();
         cozinha.setId(1L);
 
-        cadastroCozinha.remover(cozinha);
+        cozinhaRepository.remover(cozinha);
     }
 }
