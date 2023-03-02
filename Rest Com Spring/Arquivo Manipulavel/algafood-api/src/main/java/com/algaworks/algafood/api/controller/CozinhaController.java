06c5a06fb14f4f35603497a2035bd5eaa28d83e9
Rafael <rafaeldealmeida.192001@gmail.com>
2023-03-02 00:59:42 -0300

    Implementa tipo de media type

--- Rest Com Spring/Arquivo Manipulavel/algafood-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ Rest Com Spring/Arquivo Manipulavel/algafood-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -3,6 +3,7 @@ package com.algaworks.algafood.api.controller;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,14 +14,19 @@ import java.util.List;
 //@Controller
 //@ResponseBody
 @RestController //-> Ja tem o ResponseBody dentro dela
-@RequestMapping(value = "/cozinhas")
+@RequestMapping(value = "/cozinhas" /*,produces = MediaType.APPLICATION_JSON_VALUE*/)
 public class CozinhaController {
 
     @Autowired
     private CozinhaRepository cozinhaRepository;
 
-    @GetMapping
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Cozinha> listar() {
+        return cozinhaRepository.listar();
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
+    public List<Cozinha> listarNoFormatoXml() {
         return cozinhaRepository.listar();
     }
 }
