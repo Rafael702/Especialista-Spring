1f51088d41b6c8184eae4b763a5636dc2ea7ea89
Rafael <rafaeldealmeida.192001@gmail.com>
2023-03-10 13:15:15 -0300

    Add aula de correcao para busca de recursos.

--- Rest Com Spring/Arquivo Manipulavel/algafood-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ Rest Com Spring/Arquivo Manipulavel/algafood-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -4,7 +4,6 @@ import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 import com.algaworks.algafood.model.CozinhasRepresentationModel;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -38,16 +37,12 @@ public class CozinhaController {
     @GetMapping("/{cozinhaId}")
     public ResponseEntity<Cozinha> buscar(@PathVariable("cozinhaId") Long id) {
         Cozinha cozinha = cozinhaRepository.buscar(id);
-//        return ResponseEntity.status(HttpStatus.OK).body(cozinha);
-//        return ResponseEntity.ok(cozinha);
 
-        
-        HttpHeaders headers = new HttpHeaders();
-        headers.add(HttpHeaders.LOCATION, "http://api.algafood.local:8080/cozinhas");
+        if (cozinha != null) {
+            return ResponseEntity.ok(cozinha);
+        }
 
-        return ResponseEntity.status(HttpStatus.FOUND).headers(headers).build();
-
+//        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        return ResponseEntity.notFound().build();
     }
-
-
 }
