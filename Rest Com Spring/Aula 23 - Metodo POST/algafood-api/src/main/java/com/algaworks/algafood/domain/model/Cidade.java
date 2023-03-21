97a49282b753179ca4ff76cf31bb96b008811974
Rafael <rafaeldealmeida.192001@gmail.com>
2023-03-21 17:22:22 -0300

    Inclui metodo post.

--- Rest Com Spring/Aula 23 - Metodo POST/algafood-api/src/main/java/com/algaworks/algafood/domain/model/Cidade.java
+++ Rest Com Spring/Aula 23 - Metodo POST/algafood-api/src/main/java/com/algaworks/algafood/domain/model/Cidade.java
@@ -1,32 +1,30 @@
 package com.algaworks.algafood.domain.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonRootName;
+import com.sun.istack.NotNull;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
-//@JsonRootName("cozinha") -> Nome da raiz
 @Entity
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-public class Cozinha {
-
+public class Cidade {
     @EqualsAndHashCode.Include
-    @JsonIgnore
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
     private String nome;
+
+    @ManyToOne
+    @JoinColumn(name = "estado_id", nullable = false)
+    private Estado estado;
 }
