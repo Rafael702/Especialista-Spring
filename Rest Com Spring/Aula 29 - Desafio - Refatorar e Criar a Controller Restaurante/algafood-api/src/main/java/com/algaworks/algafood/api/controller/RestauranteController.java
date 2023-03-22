ae108909ee22194a0803862939133002ddb99166
Rafael <rafaeldealmeida.192001@gmail.com>
2023-03-22 16:59:09 -0300

    Inclui recurso para restaurante

---
+++ Rest Com Spring/Aula 29 - Desafio - Refatorar e Criar a Controller Restaurante/algafood-api/src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -0,0 +1,37 @@
+package com.algaworks.algafood.api.controller;
+
+import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
+import com.algaworks.algafood.domain.model.Restaurante;
+import com.algaworks.algafood.domain.service.CadastroRestauranteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/restaurantes")
+public class RestauranteController {
+
+    @Autowired
+    private CadastroRestauranteService cadastroRestauranteService;
+
+    @GetMapping
+    public ResponseEntity<List<Restaurante>> listar() {
+        List<Restaurante> restaurantes = cadastroRestauranteService.listarRestaurantes();
+        return ResponseEntity.ok().body(restaurantes);
+    }
+
+    @GetMapping("/{restauranteId}")
+    public ResponseEntity<Restaurante> buscar(@PathVariable(name = "restauranteId") Long id) {
+        try {
+            Restaurante restaurante = cadastroRestauranteService.buscarRestaurante(id);
+            return ResponseEntity.ok().body(restaurante);
+        } catch (EntidadeEmUsoException ex) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
---
+++ Rest Com Spring/Aula 29 - Desafio - Refatorar e Criar a Controller Restaurante/algafood-api/src/main/java/com/algaworks/algafood/domain/service/CadastroRestauranteService.java
@@ -0,0 +1,32 @@
+package com.algaworks.algafood.domain.service;
+
+import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
+import com.algaworks.algafood.domain.model.Restaurante;
+import com.algaworks.algafood.infraestructure.repository.RestauranteRepositoryImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CadastroRestauranteService {
+
+    @Autowired
+    private RestauranteRepositoryImpl restauranteRepository;
+
+    public List<Restaurante> listarRestaurantes(){
+        return restauranteRepository.listar();
+    }
+
+    public Restaurante buscarRestaurante(Long id){
+        Restaurante restaurante = restauranteRepository.buscar(id);
+
+        if (restaurante == null) {
+            throw new EntidadeNaoEncontradaException(String.format(
+                    "Nao existe um cadastro de cozinha com o codigo: %d", id
+            ));
+        }
+
+        return restaurante;
+    }
+}
