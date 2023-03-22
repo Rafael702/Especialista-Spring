ae73c87f7f0c7208d3de2ff4be9ccb12f1b07231
Rafael <rafaeldealmeida.192001@gmail.com>
2023-03-22 16:41:54 -0300

    Refatoracao da controller cozinha

---
+++ Rest Com Spring/Aula 29 - Desafio - Refatorar e Criar a Controller Restaurante/algafood-api/src/main/java/com/algaworks/algafood/infraestructure/repository/CozinhaRepositoryImpl.java
@@ -0,0 +1,46 @@
+package com.algaworks.algafood.infraestructure.repository;
+
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import java.util.EmptyStackException;
+import java.util.List;
+
+@Component
+public class CozinhaRepositoryImpl implements CozinhaRepository {
+    @PersistenceContext
+    private EntityManager manager;
+
+    @Override
+    public List<Cozinha> listar() {
+        return manager.createQuery("from Cozinha", Cozinha.class).getResultList();
+    }
+
+    @Transactional
+    @Override
+    public Cozinha salvar(Cozinha cozinha) {
+        return manager.merge(cozinha);
+    }
+
+    @Override
+    public Cozinha buscar(Long id) {
+        return manager.find(Cozinha.class, id);
+    }
+
+    @Transactional
+    @Override
+    public void remover(Long id) {
+        Cozinha cozinha = buscar(id);
+
+        if (cozinha == null) {
+            //Esperavamos que fosse encontrado ao menos uma cozinha com este id
+            throw new EmptyResultDataAccessException(1);
+        }
+        manager.remove(cozinha);
+    }
+}
