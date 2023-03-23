ac5d7bf31bd1b3ac3c62d3de6afa8facdebc509d
Rafael <rafaeldealmeida.192001@gmail.com>
2023-03-23 08:27:10 -0300

    Solucao para metodo Patch - Atualizacao de campos as cegas

---
+++ Rest Com Spring/Aula 34 - Solucao - Metodo Patch/algafood-api/src/main/java/com/algaworks/algafood/domain/service/CadastroCozinhaService.java
@@ -0,0 +1,66 @@
+package com.algaworks.algafood.domain.service;
+
+import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
+import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CadastroCozinhaService {
+
+    @Autowired
+    private CozinhaRepository cozinhaRepository;
+
+    public Cozinha salvar(Cozinha cozinha) {
+        return cozinhaRepository.salvar(cozinha);
+    }
+
+    public void excluir(Long id) {
+        try {
+            cozinhaRepository.remover(id);
+        } catch (EmptyResultDataAccessException ex) {
+            throw new EntidadeNaoEncontradaException(
+                    String.format(
+                            "Nao existe um cadastro de cozinha com o codigo: %d", id
+                    )
+            );
+        } catch (DataIntegrityViolationException ex) {
+            throw new EntidadeEmUsoException(
+                    String.format(
+                            "Cozinha de codigo %d nao pode ser removida, pois esta em uso", id
+                    )
+            ); //Exception de Negocio'
+        }
+    }
+
+    public List<Cozinha> listarCozinhas() {
+        return cozinhaRepository.listar();
+    }
+
+    public Cozinha buscarCozinha(Long id) {
+        Cozinha cozinha = cozinhaRepository.buscar(id);
+
+        if (cozinha == null) {
+            throw new EntidadeNaoEncontradaException(String.format(
+                    "Nao existe um cadastro de cozinha com o codigo: %d", id
+            ));
+        }
+
+        return cozinha;
+    }
+
+    public Cozinha atualizarCozinha(Long id, Cozinha updateCozinha) {
+        Cozinha cozinhaASerAtualizada = buscarCozinha(id);
+
+        BeanUtils.copyProperties(updateCozinha, cozinhaASerAtualizada, "id");
+
+        return cozinhaRepository.salvar(cozinhaASerAtualizada);
+    }
+}
---
+++ Rest Com Spring/Aula 34 - Solucao - Metodo Patch/algafood-api/src/main/java/com/algaworks/algafood/domain/service/CadastroRestauranteService.java
@@ -0,0 +1,49 @@
+package com.algaworks.algafood.domain.service;
+
+import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
+import com.algaworks.algafood.domain.model.Restaurante;
+import com.algaworks.algafood.domain.repository.RestauranteRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CadastroRestauranteService {
+
+    @Autowired
+    private RestauranteRepository restauranteRepository;
+
+    @Autowired
+    private CadastroCozinhaService cozinhaService;
+
+    public List<Restaurante> listarRestaurantes() {
+        return restauranteRepository.listar();
+    }
+
+    public Restaurante buscarRestaurante(Long id) {
+        Restaurante restaurante = restauranteRepository.buscar(id);
+
+        if (restaurante == null) {
+            throw new EntidadeNaoEncontradaException(String.format(
+                    "Nao existe um cadastro de restaurante com o codigo: %d", id
+            ));
+        }
+
+        return restaurante;
+    }
+
+    public Restaurante salvar(Restaurante restaurante) {
+        Long cozinhaId = restaurante.getCozinha().getId();
+        cozinhaService.buscarCozinha(cozinhaId);
+        return restauranteRepository.salvar(restaurante);
+    }
+
+//    public Restaurante atualizarRestaurante(Long id, Restaurante updateRestaurante) {
+//        Restaurante restauranteASerAtualizado = buscarRestaurante(id);
+//        BeanUtils.copyProperties(updateRestaurante, restauranteASerAtualizado, "id");
+//
+//        return salvar(restauranteASerAtualizado);
+//    }
+}
