435982d1fe55286d2f752608620d22bff565ea3e
Rafael <rafaeldealmeida.192001@gmail.com>
2023-03-24 18:47:58 -0300

    Inclui metodo com criterios do JPA

---
+++ Super Poderes do Spring Data JPA/Aula 6 - Usando os criterios do JPA/algafood-api/src/main/java/com/algaworks/algafood/api/controller/EstadoController.java
@@ -0,0 +1,80 @@
+package com.algaworks.algafood.api.controller;
+
+import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
+import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
+import com.algaworks.algafood.domain.model.Estado;
+import com.algaworks.algafood.domain.repository.EstadoRepository;
+import com.algaworks.algafood.domain.service.CadastroEstadoService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/estados")
+public class EstadoController {
+    @Autowired
+    private EstadoRepository estadoRepository;
+
+    @Autowired
+    private CadastroEstadoService cadastroEstadoService;
+
+    @GetMapping
+    public List<Estado> listar() {
+        return cadastroEstadoService.listarEstados();
+    }
+
+    @GetMapping("/{estadoId}")
+    public Estado buscar(@PathVariable(name = "estadoId") Long id) {
+        return cadastroEstadoService.buscarEstado(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Estado adicionar(@RequestBody Estado estado) {
+        return cadastroEstadoService.salvarEstado(estado);
+    }
+
+    @PutMapping("/{estadoId}")
+    public ResponseEntity<?> atualizar(@PathVariable(name = "estadoId") Long id, @RequestBody Estado estado) {
+        try {
+            Optional<Estado> estadoASerAtualizado = estadoRepository.findById(id);
+
+            if (estadoASerAtualizado.isPresent()) {
+                BeanUtils.copyProperties(estado, estadoASerAtualizado.get(), "id");
+
+                cadastroEstadoService.salvarEstado(estadoASerAtualizado.get());
+
+                return ResponseEntity.ok(estadoASerAtualizado.get());
+            }
+            return ResponseEntity.notFound().build();
+        } catch (EntidadeNaoEncontradaException ex) {
+            return ResponseEntity.badRequest().body(ex.getMessage());
+        }
+    }
+
+    @DeleteMapping("/{estadoId}")
+    public ResponseEntity<?> excluir(@PathVariable(name = "estadoId") Long id) {
+        try {
+            cadastroEstadoService.excluirEstado(id);
+            return ResponseEntity.noContent().build();
+        } catch (EntidadeNaoEncontradaException ex) {
+            return ResponseEntity.notFound().build();
+        } catch (EntidadeEmUsoException ex) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+        }
+    }
+
+}
